35255e257ff9f63abe029f0c0b41ece35765f63e
Klapeyron <tomasz.bartos@protonmail.com>
2017-12-20 15:46:36 +0100

    Removed reversed indexing and index modification

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,47 @@
 #[derive(Debug, PartialEq)]
 struct World {
-    data: Vec<Vec<Field>>,
+    data: Vec<Vec<State>>,
     x: usize,
-    y: usize
+    y: usize,
+    out_of_range: State
 }
 
 impl World {
     pub fn new(xsize: usize, ysize: usize) -> World {
         World {
-            data: vec![vec![Field::NormalState(0.0); xsize]; ysize],
+            data: vec![vec![State::NormalState(0.0); xsize]; ysize],
             x: xsize,
-            y: ysize
+            y: ysize,
+            out_of_range: State::ProhibitedState
         }
     }
-    pub fn set_state(self: &mut World, new_state: Field, x: usize, y: usize) {
-        let x_index = x - 1;
-        let y_index = self.y - y;
 
-        match (&self.data[y_index][x_index], &new_state)
+    pub fn set_state(self: &mut World, new_state: State, x: usize, y: usize) {
+        match (&self.data[y][x], &new_state)
         {
-            (&Field::NormalState(_), &Field::NormalState(_)) => {
-                println!("Updated value at [{}][{}] from {:?} to {:?}", x, y, &self.data[y_index][x_index], &new_state);
+            (&State::NormalState(_), &State::NormalState(_)) => {
+                println!("Updated value at [{}][{}] from {:?} to {:?}", x, y, &self.data[y][x], &new_state);
             }
-            (&Field::NormalState(_), _) => {
+            (&State::NormalState(_), _) => {
                 println!("Created {:?} on position [{}][{}]", &new_state, x, y);
             }
             _ => {
                 panic!("Invalid operation {:?} on position [{}][{}]", new_state, x, y);
             }
         }
-        self.data[y_index][x_index] = new_state;
+        self.data[y][x] = new_state;
     }
-    pub fn read_state(self: &World, x: usize, y: usize) -> &Field {
-        return &self.data[self.y-y][x-1];
+
+    pub fn read_state(self: &World, x: usize, y: usize) -> &State {
+        if x < self.x && y < self.y {
+            return &self.data[y][x];
+        }
+        else {
+            return &self.out_of_range;
+        }
     }
-    pub fn walk(self: &World, predicate: &mut FnMut(&Field, usize, usize)) {
+
+    pub fn walk(self: &World, predicate: &mut FnMut(&State, usize, usize)) {
         for (x, row) in self.data.iter().enumerate() {
             for (y, column) in row.iter().enumerate() {
                 predicate(column, x+1, y+1);
@@ -44,7 +51,7 @@ impl World {
 }
 
 #[derive(Debug, Clone, PartialEq)]
-enum Field {
+enum State {
     ProhibitedState,
     StartState,
     TerminalState(f64),
@@ -52,17 +59,35 @@ enum Field {
     NormalState(f64)
 }
 
-fn evaluate(world: &mut World, world_x: usize, world_y: usize){
-    world.set_state(Field::TerminalState(1.0), 4, 3);
+#[derive(Debug, Clone, PartialEq)]
+enum Action {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+fn model(_state: State, action: Action, _new_state: State) -> f64 {
+    match action {
+        Action::Up => 0.8,
+        Action::Left => 0.1,
+        Action::Right => 0.1,
+        Action::Down => 0.0
+    }
+}
+
+fn evaluate(world: &mut World, x: usize, y: usize){
+    let world_size_x = 4;
+    let world_size_y = 3;
 }
 
 fn main() {
     let mut world: World = World::new(4, 3);
 
-    world.set_state(Field::StartState, 1, 1);
-    world.set_state(Field::ProhibitedState, 2, 2);
-    world.set_state(Field::TerminalState(1.0), 4, 3);
-    world.set_state(Field::TerminalState(-1.0), 4, 2);
+    world.set_state(State::StartState, 1, 1);
+    world.set_state(State::ProhibitedState, 2, 2);
+    world.set_state(State::TerminalState(1.0), 4, 3);
+    world.set_state(State::TerminalState(-1.0), 4, 2);
 
     println!("Hello, {:#?}", world);
 }
@@ -70,38 +95,23 @@ fn main() {
 #[test]
 fn correct_indexing() {
     let mut world: World = World::new(4, 3);
-    world.set_state(Field::TerminalState(1.0), 4, 3);
+    world.set_state(State::TerminalState(1.0), 3, 2);
 
     assert_eq!(4, world.x);
     assert_eq!(3, world.y);
 }
 
 #[test]
-fn save_on_correct_positions() {
-    let mut world: World = World::new(4, 3);
-
-    world.set_state(Field::StartState, 1, 1);
-    world.set_state(Field::ProhibitedState, 2, 2);
-    world.set_state(Field::TerminalState(1.0), 4, 3);
-    world.set_state(Field::TerminalState(-1.0), 4, 2);
-
-    assert_eq!(Field::StartState, world.data[2][0]);
-    assert_eq!(Field::ProhibitedState, world.data[1][1]);
-    assert_eq!(Field::TerminalState(1.0), world.data[0][3]);
-    assert_eq!(Field::TerminalState(-1.0), world.data[1][3]);
-}
-
-#[test]
 fn update_normal_state() {
     let mut world: World = World::new(4, 3);
 
-    assert_eq!(&Field::NormalState(0.0), world.read_state(1,1));
+    assert_eq!(&State::NormalState(0.0), world.read_state(1,1));
 
-    world.set_state(Field::NormalState(4.2), 1,1);
-    assert_eq!(&Field::NormalState(4.2), world.read_state(1,1));
+    world.set_state(State::NormalState(4.2), 1,1);
+    assert_eq!(&State::NormalState(4.2), world.read_state(1,1));
 
-    world.set_state(Field::NormalState(6.6), 1,1);
-    assert_eq!(&Field::NormalState(6.6), world.read_state(1,1));
+    world.set_state(State::NormalState(6.6), 1,1);
+    assert_eq!(&State::NormalState(6.6), world.read_state(1,1));
 }
 
 #[test]
@@ -109,8 +119,8 @@ fn update_normal_state() {
 fn not_allow_normal_state_update_from_different_type() {
     let mut world: World = World::new(4, 3);
 
-    world.set_state(Field::ProhibitedState, 1, 1);
-    world.set_state(Field::NormalState(4.2), 1, 1);
+    world.set_state(State::ProhibitedState, 1, 1);
+    world.set_state(State::NormalState(4.2), 1, 1);
 }
 
 #[test]
@@ -118,51 +128,45 @@ fn not_allow_normal_state_update_from_different_type() {
 fn not_allow_immutable_state_update() {
     let mut world: World = World::new(4, 3);
 
-    world.set_state(Field::StartState, 1, 1);
-    world.set_state(Field::StartState, 1, 1);
+    world.set_state(State::StartState, 1, 1);
+    world.set_state(State::StartState, 1, 1);
 }
 
 #[test]
 fn prepare_standard_world() {
     let mut world: World = World::new(4, 3);
 
-    world.set_state(Field::StartState, 1, 1);
-    world.set_state(Field::ProhibitedState, 2, 2);
-    world.set_state(Field::TerminalState(1.0), 4, 3);
-    world.set_state(Field::TerminalState(-1.0), 4, 2);
+    world.set_state(State::StartState, 0, 0);
+    world.set_state(State::ProhibitedState, 1, 1);
+    world.set_state(State::TerminalState(1.0), 3, 2);
+    world.set_state(State::TerminalState(-1.0), 3, 1);
 
-    assert_eq!(&Field::StartState, world.read_state(1,1));
-    assert_eq!(&Field::ProhibitedState, world.read_state(2,2));
-    assert_eq!(&Field::TerminalState(1.0), world.read_state(4,3));
-    assert_eq!(&Field::TerminalState(-1.0), world.read_state(4,2));
+    assert_eq!(&State::StartState, world.read_state(0,0));
+    assert_eq!(&State::ProhibitedState, world.read_state(1,1));
+    assert_eq!(&State::TerminalState(1.0), world.read_state(3,2));
+    assert_eq!(&State::TerminalState(-1.0), world.read_state(3,1));
 }
 
 #[test]
-#[should_panic]
-fn panic_if_accessed_out_of_range_x_range() {
+fn return_prohibited_state_if_accessed_out_of_range() {
     let world: World = World::new(4, 3);
-    world.read_state(5,3);
-}
-
-#[test]
-#[should_panic]
-fn panic_if_accessed_out_of_range_y_range() {
-    let world: World = World::new(4, 3);
-    world.read_state(4,4);
+    assert_eq!(&State::ProhibitedState, world.read_state(5,3));
+    assert_eq!(&State::ProhibitedState, world.read_state(4,4));
+    assert_eq!(&State::ProhibitedState, world.read_state(4,4));
 }
 
 #[test]
 #[should_panic]
 fn panic_if_inserted_out_of_range_x_range() {
     let mut world: World = World::new(4, 3);
-    world.set_state(Field::StartState, 5, 3);
+    world.set_state(State::StartState, 5, 3);
 }
 
 #[test]
 #[should_panic]
 fn panic_if_inserted_out_of_range_y_range() {
     let mut world: World = World::new(4, 3);
-    world.set_state(Field::StartState, 4, 4);
+    world.set_state(State::StartState, 4, 4);
 }
 
 #[test]
@@ -170,7 +174,7 @@ fn walk_through_all_fields() {
     let world: World = World::new(4, 3);
     let mut numberOfCalls = 0;
 
-    let mut walk_predicate = |field: &Field, x: usize, y: usize|
+    let mut walk_predicate = |_field: &State, x: usize, y: usize|
     {
         numberOfCalls += 1;
         println!("Iterating over {} {}", x, y);
